e6a45ea5de1535653d9ff4c4bffb39d05d4a9d61
Ian Gagliardi <iangagliardi@gmail.com>
2025-10-02 09:15:27 +0200

    Exercise update

--- Exercises/00_pandas_basic_syntax/1- Cities in Sweden - create dataset.py
+++ Exercises/00_pandas_basic_syntax/1- Cities in Sweden - create dataset.py
@@ -1,2 +1,19 @@
 # Cities in Sweden - create dataset
+import pandas as pd
 
+cities = {"Kommun": ["Malmö", "Stockholm", "Uppsala", "Göteborg"],
+          "Population": [347949, 975551, 233839, 583056]}
+df = pd.DataFrame(cities)
+print(f"{df}\n")
+
+filt = df['Kommun'] == 'Göteborg'
+print(f"{df[filt]}\n")
+
+print(f"{df.sort_values('Population', ascending=False)}\n")
+
+print(f"{df.sort_values('Population', ascending=False).head(3)}\n")
+
+sweden_population = 10379295
+
+df['Population (%)'] = (df['Population'] / sweden_population) * 100
+print(f"{df.sort_values('Population', ascending=False)}\n")
